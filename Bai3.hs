10764d0515ec9de4856cc46c3803ee04c0310fdd
hoaitrungchar <79318706+hoaitrungchar@users.noreply.github.com>
2023-03-27 21:32:45 +0700

    lam bai 3

--- Bai3.hs
+++ Bai3.hs
@@ -1,4 +1,6 @@
 elementAt :: [x] -> Int -> x
 elementAt (x:xs) 1= x 
 elementAt [] k = error "Index out of bound"
-elementAt (x:xs) k =elementAt xs (k-1)
+elementAt (_:xs) k
+  | k < 1           = error "Index out of bounds"
+  | otherwise       = elementAt' xs (k - 1)
